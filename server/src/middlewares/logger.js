c2f7a9d3c8d0b4471eca429a738f318fc888549b
Myls Blanza <mylsblanza290@gmail.com>
2026-03-19 21:49:55 +0100

    feat(middleware): agregar logger para registrar todas las peticiones HTTP

---
+++ server/src/middlewares/logger.js
@@ -0,0 +1,7 @@
+function logger(req, _res, next) {
+    const now = new Date().toISOString();
+    console.log(`[${now}] ${req.method} ${req.originalUrl}`);
+    next();
+}
+
+module.exports = logger;
