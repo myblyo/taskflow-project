6961d2c024eb45361d54663514de1cacfb6b93aa
Myls Blanza <mylsblanza290@gmail.com>
2026-03-19 21:50:05 +0100

    feat(middleware): agregar manejador global de errores para la API

---
+++ server/src/middlewares/errorHandler.js
@@ -0,0 +1,8 @@
+function errorHandler(err, _req, res, _next) {
+    console.error(err);
+    res.status(500).json({
+        error: 'Internal server error'
+    });
+}
+
+module.exports = errorHandler;
