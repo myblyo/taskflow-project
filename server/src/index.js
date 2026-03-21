36bb598dbf14241adf8c2ec1b78395c0f4fd41c9
Myls Blanza <mylsblanza290@gmail.com>
2026-03-21 17:49:33 +0100

    feat: crear middleware

--- server/src/index.js
+++ server/src/index.js
@@ -3,17 +3,26 @@ const app = express();
 const { PORT } = require('./config/env');
 const taskRoutes = require('./routes/task.routes');
 const logger = require('./middlewares/logger');
-const errorHandler = require('./middlewares/errorHandler');
+const cors = require('cors');
 
 // Middlewares base
 app.use(logger);
 app.use(express.json());
+app.use(cors());
 
 // Prefijo para las rutas de tareas
 app.use('/api/v1/tasks', taskRoutes);
 
-// Handler de errores (siempre al final)
-app.use(errorHandler);
+/**
+ * Middleware final de errores (obligatorio: 4 argumentos para que Express lo reconozca).
+ * Captura errores pasados con next(err) desde rutas/controladores.
+ */
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({
+        error: 'Internal server error'
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
