e9bf054eacc32714df1ec1d400d643495f08779c
Myls Blanza <mylsblanza290@gmail.com>
2026-03-21 19:08:50 +0100

    refactor(tareas): eliminar exports de funciones antiguas con validación simulada

--- server/src/routes/task.routes.js
+++ server/src/routes/task.routes.js
@@ -3,38 +3,8 @@ const router = express.Router();
 
 const taskController = require('../controllers/task.controller');
 
-// Ruta para crear una nueva tarea
 router.post('/', taskController.crearTarea);
-
-// Ruta para obtener todas las tareas
 router.get('/', taskController.obtenerTodas);
-
-// Ruta para eliminar una tarea
 router.delete('/:id', taskController.eliminarTarea);
 
 module.exports = router;
-
-
-exports.eliminarTarea = (req, res, next) => {
-    const { id } = req.params;
-
-    const existeTarea = false; // Simulación de validación
-
-    if (!existeTarea) {
-        return  next(new Error('NOT_FOUND')); // Pasar error al middleware de errores
-    }
-
-    res.json({ message: 'Tarea eliminada exitosamente' });
-};
-
-exports.crearTarea = (req, res, next) => {
-    const { title, description } = req.body;
-
-    // Validación
-    if (!title) {
-        return next(new Error('FALTA_TITULO')); // Pasar error al middleware de errores
-    }
-
-    res.json({ message: 'Tarea creada exitosamente' });
-
-};
