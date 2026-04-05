4ee3f57b6b5db67542d7293a7f87e4b36c222163
Myls Blanza <mylsblanza290@gmail.com>
2026-04-05 17:32:16 +0200

    feat(routes): registrar PATCH /:id para actualización de tareas

--- server/src/routes/task.routes.js
+++ server/src/routes/task.routes.js
@@ -5,6 +5,7 @@ const taskController = require('../controllers/task.controller');
 
 router.post('/', taskController.crearTarea);
 router.get('/', taskController.obtenerTodas);
+router.patch('/:id', taskController.actualizarTarea);
 router.delete('/:id', taskController.eliminarTarea);
 
 module.exports = router;
